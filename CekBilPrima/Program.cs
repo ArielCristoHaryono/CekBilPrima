f70386d610955d86a63293379d8a2214aef3c60f
ArielCristoHaryono <aharyono01@student.ciputra.ac.id>
2021-12-03 10:33:01 +0700

    Add project files.

---
+++ CekBilPrima/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CekBilPrima
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cek Bilangan Prima");
+            Console.WriteLine("masukkan input angka");
+            int input = (Convert.ToInt32(Console.ReadLine()));
+            if (input mod 1 == 0)
+                {
+                Console.WriteLine("Bilangan Prima ")
+                }
+            else (input mod 1!==0)
+                {
+                Console.WriteLine("Bukan Bilangan Prima")
+                }
+        }
+    }
+}
